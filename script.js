055698b82acbd9174808397348379f9d9a6fae1a
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2023-11-29 17:21:57 +0100

    index.html

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  const scrollToSection = (sectionId) => {
+      const section = document.getElementById(sectionId);
+      section && section.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  // Event listener for the menu links and logo
+  const menuLinks = document.querySelectorAll('.menu-link');
+  const logo = document.getElementById('logo');
+
+  menuLinks.forEach(link => {
+      link.addEventListener('click', (event) => {
+          event.preventDefault();
+          scrollToSection(link.getAttribute('href').substring(1));
+      });
+  });
+
+  // Logo click event
+  logo && logo.addEventListener('click', () => {
+      scrollToSection('home');
+  });
+
+  // Event listener for the menu button
+  const menuButton = document.getElementById('menu');
+  menuButton && menuButton.addEventListener('click', () => {
+      const nav = document.getElementById('nav');
+      nav && nav.classList.toggle('show');
+  });
+
+
+
+});
